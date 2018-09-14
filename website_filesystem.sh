55e9e8a5b309ae831035f2e688d4b33c5f29b476
cinseire <cinseire.washington@hgs.hiddengeniusproject.org>
2018-09-13 18:31:43 -0700

    first commit

---
+++ website_filesystem.sh
@@ -0,0 +1,25 @@
+pwd							#print working directory
+cd ~/Desktop				#took me to my Desktop folder
+Mkdir Web_filesystem	 	#making a folder
+cd Web_filesystem			#takes you to web_filesytem folder
+touch index.html			# creates a file called index.html
+mkdir html css js img extra		#creates html folder, css folder, js folder, img folder, and a extra folder
+cd html							# goes to html folder
+touch pg1.html					#creates pg1.html folder
+touch pg2.html					#creates pg2.html folder
+cd ../css						#takes you from html folder to css folder
+touch pg1.css					#creates pg1.css file
+touch pg2.css					#creates pg2.css file
+touch index.css					#creates index.css file
+cd ../js						#takes you to js folder
+touch pg1.js					#creates pg1.js file
+touch pg2.js					#creates pg2.js file
+touch index.js					#creates index.js file
+cd ../extra						#takes you from js folder to extra folder
+mkdir audio						#creates  audio folder in extra folder
+mkdir video						##creates  video
+cd ../img						#goes to img folder
+curl -o img-1.jpg "http://catalyst.microsoftbayarea.com/wp-content/uploads/2015/05/Yes-We-Code_2048-942.jpg"			#dowloads and name image from the internet
+curl -o img-2.jpg "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/ISR-HW57.png/250px-ISR-HW57.png"			#dowloads and name image from the internet
+curl -o img-3.jpg "https://www.umami.site/wp-content/uploads/2017/08/Sous-Vide-Roast-Beed-Featured.jpg"					#dowloads and name image from the internet
+
